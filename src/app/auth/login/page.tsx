492ad86b618ac4849f893b7469fc82959887903d
Maren <maren@develov.com>
2024-09-01 21:10:15 +0200

    Initial commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import { titleFont } from "@/config/fonts";
+import Image from "next/image";
+
+export default function Login() {
+    return (
+        <main className="">
+            <h1>LoginPage</h1>
+            <h1 className={titleFont.className}>Hola Mundo</h1>
+        </main>
+    );
+}
---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+
+
+
+export * from './ui/not-found/PageNotFound';
+export * from './ui/title/Title';
+export * from './ui/top-menu/TopMenu';
+export * from './ui/sidebar/SideBar';
+export * from './products/product-grid/ProductGrid';
+export * from './products/product-grid/ProductGridItem';
